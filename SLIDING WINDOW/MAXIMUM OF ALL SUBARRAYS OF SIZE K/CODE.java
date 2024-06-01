49de2e2c096299761be1dcd7b6c64136b2f15df3
Newtanmukh <importantnewton@gmail.com>
2024-06-01 15:46:29 +0530

    added

--- SLIDING WINDOW/MAXIMUM OF ALL SUBARRAYS OF SIZE K/CODE.java
+++ SLIDING WINDOW/MAXIMUM OF ALL SUBARRAYS OF SIZE K/CODE.java
@@ -1,29 +1,30 @@
-  static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+class Solution
+{
+    //Function to find maximum of each subarray of size k.
+    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
     {
 
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        Deque<Integer> dq = new ArrayDeque<>();
-        
-        for(int i=0; i<n ; i++){
-            
-
-            if(!dq.isEmpty() && dq.peekFirst() <= i-k){
-                dq.poll();
-            }
-            
-            while(!dq.isEmpty() && arr[dq.peekLast()] <= arr[i]){
-                dq.pollLast();
-            }
-            
-    
-            dq.offer(i);
-            
-            if(i >= k-1){
-                list.add(arr[dq.peek()]);
-            }
-        }
-        
-        return list;
-        
+       ArrayList<Integer> list = new ArrayList<Integer>();
+       Deque<Integer> dq = new ArrayDeque<Integer>();
+       
+       for(Integer i=0; i<n; i++){
+           
+           while(dq.size() > 0 && dq.peekFirst() < i-k+1){
+               dq.removeFirst();
+           }
+           
+           while(dq.size() > 0 && arr[dq.peekLast()] <= arr[i]){
+               dq.removeLast();
+           }
+           
+           dq.addLast(i);
+           
+           if(i >= k-1){
+               list.add(arr[dq.peekFirst()]);
+           }
+       }
+       
+       return list;
+       
     }
+}
