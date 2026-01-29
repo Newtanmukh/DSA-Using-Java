6a3c4f7c6f6a7ee83cd59c149764847ec36a4360
Newtanmukh <190010027@iitdh.ac.in>
2026-01-29 10:26:11 +0530

    copunt of sum of consecutives added.

--- MATH/(V.IMP.)COUNT OF SUM OF CONSECUTIVES/Code.java
+++ MATH/(V.IMP.)COUNT OF SUM OF CONSECUTIVES/Code.java
@@ -1,5 +1,6 @@
 class Solution {
-
+//the thing is that we will use 2*n>k*(k-1) and NOT 2*n>=k*(k-1).
+//This is because, if it equals zero then the sequence will start from 0, which is not corect.
     int getCount(int n) {
         // code here
         int ans = 0;
