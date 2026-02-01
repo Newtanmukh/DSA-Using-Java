73ddc9a88c55e00fbb537933daf6cb2a58cbe19a
Newtanmukh <190010027@iitdh.ac.in>
2026-02-01 12:14:59 +0530

    TRIE QUestion done.

---
+++ TRIE/(V.V.V.V.V.IMP.) REPLACE WORDS/CODE.java
@@ -0,0 +1,69 @@
+class Solution {
+    Trie root;
+
+    class Trie{
+        public Trie[] children;
+        public boolean isEnd;
+
+        public Trie(){
+            children = new Trie[26];
+            isEnd = false;
+        }
+    }
+
+    public String replaceWords(List<String> dict, String sentence) {
+        root = new Trie();
+
+        for(String word: dict){
+            insert(word);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String[] input = sentence.split(" ");
+
+        for(String i: input){
+            sb.append(search(i));
+            sb.append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+
+    public void insert(String word){
+        Trie node = root;
+        char[] ch = word.toCharArray();
+        // cat
+        // root -> 'c' -> 'a' -> 't', 't' node is end and this isEnd = true.
+        for(char c: ch){
+            int i = c - 'a';
+
+            if(node.children[i] == null){
+                node.children[i] = new Trie();
+            }
+
+            node = node.children[i];
+        }
+        node.isEnd = true;
+    }
+
+    public String search(String word){
+        Trie node = root;
+        char[] ch = word.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(char c: ch){
+            int i = c - 'a';
+            if(node.children[i]==null){
+                return word;
+            }else if(node.children[i].isEnd){
+                sb = sb.append(c);
+                return sb.toString();
+            }else{
+                sb = sb.append(c);
+                node = node.children[i];
+            }
+        }
+
+        return word;
+    }
+
+}
