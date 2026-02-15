71adf712710cffed67c6bd68d19e681c41973f8a
Newtanmukh <190010027@iitdh.ac.in>
2026-02-15 11:25:43 +0530

    BUBBLE SORT.

---
+++ ARRAYS  SEARCH SORTING/BUBBLE SORT/CODE.java
@@ -0,0 +1,17 @@
+//problem desc. : https://www.geeksforgeeks.org/problems/bubble-sort/1
+
+class Solution {
+    public void bubbleSort(int[] arr) {
+
+        for(int i = 0; i < arr.length;i++){
+            for(int j = i;j < arr.length;j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+    }
+}
